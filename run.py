ca4f821b107f81fc920d9fbc7d6379d06999d6e5
Madde <Madde.frey@gmail.com>
2021-11-16 15:52:20 +0000

    add options in function update_available_shee for user to choose

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
+from prettytable import PrettyTable
+table = PrettyTable()
+x = PrettyTable()
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -13,26 +16,35 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('adoption_page')
 
+
 def available_table():
     """
-    t
+    Display details of all available animals in the available worksheet,
+    sheet as a table using PrettyTables
     """
     print("\033c")
     available = SHEET.worksheet("available").get_all_values()
-    pprint(available)
+    table.field_names = ['NAME', 'AGE', 'SPECIE', 'STATUS']
+    table.add_rows(available)
+
+    print(table)
 
     main_menu_available = input("\n Press enter to return to main page")
 
 
 def past_table():
     """
-    t
+    Display details of all past animals in the past worksheet,
+    sheet as a table using PrettyTables
     """
     print("\033c")
     past = SHEET.worksheet("past").get_all_values()
-    pprint(past)
+    x.field_names = ['NAME', 'AGE', 'SPECIE', 'STATUS']
+    x.add_rows(past)
+    print(x)
 
     main_menu_past = input("\n Press enter to return to main page")
+
 
 def update_database():
     """
@@ -57,7 +69,7 @@ def update_database():
 
         elif user_choice == '3':
             print('You picked 3')
-            
+            update_available_sheet()
 
         elif user_choice == '4':
             print('you picked 4')
@@ -66,6 +78,7 @@ def update_database():
             print("invalid answer")
             #input('please enter a number from 1-4 here: ')
     
+
 def add_animal():
     """
     t
@@ -80,7 +93,6 @@ def add_animal():
         add_animal_data = add_animal_input.split(",")
 
         if validate_add_data(add_animal_data):
-            #call the update_add_animal_worksheet function instead?
             print('valid')
             available_worksheet = SHEET.worksheet('available')
             available_worksheet.append_row(add_animal_data)
@@ -95,7 +107,6 @@ def validate_add_data(values):
     there need to be exactly 4 values
     """
     print("\033c")
-    #print(values)
     try:
         #[int(value) for value in values]
         if len(values) != 4:
@@ -108,20 +119,47 @@ def validate_add_data(values):
 
     return True
 
-def update_add_animal_worksheet(data):
+
+def update_available_sheet():
     """
     t
     """
-    #print("updating available worksheet... \n")
-    #available_worksheet = SHEET.available
-    #available_worksheet.append_row(data)
-    #print('it is working \n')
+    print("\033c")
+    while True:
+        print("Introduction to update page \n")
+        print("1. Delete available animal")
+        print("2. Move available animal to past")
+        print("3. Back to main page/Exit \n")
+
+        update_choice = input('\n please enter a number from 1-4 here: ')
+
+        if update_choice == '1':
+            print("works")
+            available_table()
+        elif update_choice == '2':
+            print('You picked 2')
+            add_animal()
+
+        elif update_choice == '3':
+            print('You picked 3')
+            get_user_data()
+        else:
+            print("invalid answer")
+
+
+def delete_row():
+    """
+    y
+    """
+    
+
+
 
 def get_user_data():
     """
-    Get User data here
+    Get User input data here
     """
-    
+    print("\033c")
     while True:
         print("Introduction to adoption page \n")
         print("1. Show available animals")
